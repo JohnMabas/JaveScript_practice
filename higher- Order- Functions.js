f453eed5e033e6dce1e18f249d0b8b7bc387ba9f
mabas <johnmabas1@gmail.com>
2026-07-02 00:47:14 +0100

    filter

--- higher- Order- Functions.js
+++ higher- Order- Functions.js
@@ -15,12 +15,34 @@
 
 let numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach((number) => console.log(number + 1));
+// numbers.forEach((number) => console.log(number + 1));
 
 
 let arr = [5, 10, 15];
 
 arr.forEach((item)=>{
-  console.log(item * 2); //10, 20, 30
+  // console.log(item * 2); //10, 20, 30
 });
-console.log(arr);
+// console.log(arr);
+
+
+// 2. create a new array that only has the odd numbers from the numbers array
+ 
+// Using filter()......
+
+let arrNum = [1, 2, 3, 4, 5];
+
+let oddArray = arrNum.filter((number) => number % 2 !== 0);
+// console.log(oddArray);
+
+/////
+let arr1 = ['aaa', 'bbbb', 'ccccc'];
+
+arr1.filter((item, index)=>{
+  if (item.length < 3){
+    return true;
+  } else {
+    return false;
+  }
+  console.log(index);
+});
