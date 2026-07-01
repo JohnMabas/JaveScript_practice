fca1ae445e0a1601102a4049278fc36c388ecb0e
mabas <johnmabas1@gmail.com>
2026-07-01 11:46:37 +0100

    forEach()

---
+++ higher- Order- Functions.js
@@ -0,0 +1,26 @@
+// 1. add one to each number in an array and display it in the console
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// function addOne(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     console.log(array[i] + 1);
+//   }
+// }
+
+// addOne(numbers);
+
+
+// Using forEach().....
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((number) => console.log(number + 1));
+
+
+let arr = [5, 10, 15];
+
+arr.forEach((item)=>{
+  console.log(item * 2); //10, 20, 30
+});
+console.log(arr);
