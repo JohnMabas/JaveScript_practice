00c495afedd8ac3278573a77d4ddafc6d5c4a935
mabas <johnmabas1@gmail.com>
2026-07-10 23:26:57 +0100

    string method

--- string-Method.js
+++ string-Method.js
@@ -8,3 +8,19 @@ console.log("JavaScript".toUpperCase())
 console.log("JavaScript".toLowerCase())
 console.log("JavaScript".slice(2,-5))
 console.log("Mabas".concat(" John"))
+
+let lang = "Java_JavaScript_Python";
+let arr = lang.split("-")
+console.log(arr[0])
+
+console.log("JavaScript".includes("va"))
+console.log(" i love JavaScript".replace("JavaScript", "PHP"))
+console.log("01-01-2026".replaceAll("-", "/"))
+console.log(" hello js ".trim())
+console.log("Dev".padEnd(10,"*"))
+console.log("Dev".padStart(10,"*"))
+console.log("JavaScript".startsWith("J"))
+console.log("JavaScript".endsWith("t"))
+console.log("John is a boy".indexOf("s"))
+
+
