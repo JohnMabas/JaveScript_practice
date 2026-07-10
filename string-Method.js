5b6dd992b3533c22f19431a925f2a39e84d1aa86
mabas <johnmabas1@gmail.com>
2026-07-10 22:44:47 +0100

    string method

---
+++ string-Method.js
@@ -0,0 +1,10 @@
+
+let str = "Mabas";
+console.log(str.length)
+
+console.log("JavaScript".charAt(3))
+console.log("JavaScript".charCodeAt(3))
+console.log("JavaScript".toUpperCase())
+console.log("JavaScript".toLowerCase())
+console.log("JavaScript".slice(2,-5))
+console.log("Mabas".concat(" John"))
