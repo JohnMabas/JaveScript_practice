e2e15b50e1b7852efeaacacdba7fa2564d10d2e0
mabas <johnmabas1@gmail.com>
2026-07-11 01:21:08 +0100

    obect manipulation

--- object-Manipulation.js
+++ object-Manipulation.js
@@ -32,4 +32,60 @@ function createUser(name, age){
 const user1 = createUser("John", 33)
 console.log(user1)
 
+let profile = {
+    name: "mabas",
+    company: "CreoWis",
+    message: function() {
+        console.log(`${this.name} work at ${this.company}`)
+    },
+    address: {
+        city:"Banglore",
+        pin: 56032,
+        state: "Jos",
+        country: "India",
+        greeting: function() {
+            console.log("Welcome in India")
+        }
+    },
+    // salary: 100
+}
+
+// for ... in
+
+for (let key in profile){
+    console.log(key)
+    console.log(profile[key])
+}
+
+console.log(profile.salary)
+
+if(!profile.salary) {
+    console.log("The salary property doesn't exist")
+}
+
+profile.address.greeting()
+console.log(profile.name)
+console.log(profile.company)
+
+
+profile.message()
+
+
+// Object Reference..
+
+let fruit = { name: "mango"}
+const oneMoreFruit = { name: "mango"};
+
+
+console.log(fruit == oneMoreFruit)
+console.log(fruit === oneMoreFruit)
+
+fruit = oneMoreFruit;
+
+console.log(fruit == oneMoreFruit)
+console.log(fruit === oneMoreFruit)
+
+
+
+
 
