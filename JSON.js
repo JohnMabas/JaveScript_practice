786217a1315d29fa916a50edf89309eec33e4aaf
mabas <johnmabas1@gmail.com>
2026-07-16 22:53:55 +0100

    JSON task

--- JSON.js
+++ JSON.js
@@ -1,6 +1,18 @@
 
-let user = ['{"name": "mabas", "age": 21, "location": "Jos"}'];
-let obj = JSON.parse(user);
-let res = `My name is ${obj.name} and am ${obj.age} years old, and am located in ${obj.location}`;
+// let user = ['{"name": "mabas", "age": 21, "location": "Jos"}'];
+// let obj = JSON.parse(user);
+// let res = `My name is ${obj.name} and am ${obj.age} years old, and am located in ${obj.location}`;
 
-console.log(res)
+// console.log(res)
+
+
+const car = {
+  brand: "Benz",
+  model: "E250",
+  year: 2023,
+  owner: {
+    name: "Mabas",
+    city: "Jos"
+  },
+  features: ["Airbags", "Radio", "Camera", "Bluetooth", "Electronic Stability Program (ESP),"]
+};
