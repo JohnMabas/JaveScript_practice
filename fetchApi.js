6c1d0848f4d9d73f8993e8df775254e922bfa03c
mabas <johnmabas1@gmail.com>
2026-07-17 00:45:32 +0100

    fetch API

---
+++ fetchApi.js
@@ -0,0 +1,13 @@
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+.then(response => {
+    if(!response.ok){
+        throw new Error("Could not fetch resource");
+
+    }
+    return response.json();
+
+})
+.then(data => console.log(data.id))
+.catch(error => console.log(error) );
+
+
