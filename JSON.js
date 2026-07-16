3a5086dbca29e4e1e5e2ce73b6713236403a30dc
mabas <johnmabas1@gmail.com>
2026-07-16 23:19:59 +0100

    JSON task

--- JSON.js
+++ JSON.js
@@ -1,10 +1,8 @@
-
 // let user = ['{"name": "mabas", "age": 21, "location": "Jos"}'];
 // let obj = JSON.parse(user);
 // let res = `My name is ${obj.name} and am ${obj.age} years old, and am located in ${obj.location}`;
 
 // console.log(res)
-
 
 const car = {
   brand: "Benz",
@@ -12,7 +10,26 @@ const car = {
   year: 2023,
   owner: {
     name: "Mabas",
-    city: "Jos"
+    city: "Jos",
   },
-  features: ["Airbags", "Radio", "Camera", "Bluetooth", "Electronic Stability Program (ESP),"]
+  features: [
+    "Airbags",
+    "Radio",
+    "Camera",
+    "Bluetooth",
+    "Electronic Stability Program (ESP),"
+  ],
 };
+
+let jsonData = JSON.stringify(car, null, 2);
+console.log(jsonData);
+
+let newCar = JSON.parse(jsonData);
+console.log("Owner Name:", newCar.owner.name);
+console.log("Second Feature:", newCar.features[1]);
+
+if (car.brand === newCar.brand) {
+    console.log("The brands are the same.");
+} else {
+    console.log("The brands are different.");
+}
